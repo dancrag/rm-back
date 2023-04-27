80f1e9436cec50aae1cc3254cc15ad21c32ec9cb
dancrag <39243966+dancrag@users.noreply.github.com>
2023-04-27 23:48:20 +0000

    changed fetch for axios

--- src/controllers/characters.js
+++ src/controllers/characters.js
@@ -6,24 +6,21 @@ router.get("/byPage/:page", (req, res) => {
     const page = req.params.page || 1
 
     getCharactersByPage(page)
-        .then(response => response.json())
-        .then(data => res.send(data))
+        .then(response => res.send(response))
 })
 
 router.get("/:id", (req, res) => {
     const characterId = req.params.id || 1
     
     getCharacterById(characterId)
-        .then(response => response.json())
-        .then(data => res.send(data))
+        .then(response => res.send(response))
 })
 
 router.get("/byName/:name", (req, res) => {
     const characterName = req.params.name || ""
 
     getCharacterByName(characterName)
-        .then(response => response.json())
-        .then(data => res.send(data))
+        .then(response => res.send(response))
 })
 
 module.exports = router;
--- src/services/characters.js
+++ src/services/characters.js
@@ -1,4 +1,4 @@
-const fetch = require("node-fetch")
+const axios = require("axios")
 const SLASH_PARAM = "/?"
 const SLASH = "/"
 const PAGE_PARAM = "page="
@@ -7,7 +7,7 @@ const BASE_URL = process.env.BASE_URL_RICK_AND_MORTY
 const CHARACTER_URI = process.env.CHARACTER_URI
 
 const getCharactersByPage = (page) => {
-    return fetch(BASE_URL + CHARACTER_URI + SLASH_PARAM + PAGE_PARAM + page)
+    return axios.get(BASE_URL + CHARACTER_URI + SLASH_PARAM + PAGE_PARAM + page)
 }
 
 const getCharacterById = (id) => {
