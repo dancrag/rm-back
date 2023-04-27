7dd92f5f6c38dadfcce247858dfed5cb1f75c85a
dancrag <39243966+dancrag@users.noreply.github.com>
2023-04-27 22:56:44 +0000

    added netlify functions capability

--- src/api.js
+++ src/api.js
@@ -4,11 +4,15 @@ const cors = require("cors")
 const serverless = require("serverless-http")
 const characterRouter = require("./controllers/characters")
 
+const router = express.Router()
+
 const app = express()
-const PORT = 2896 || process.env.PORT
 
 app.use(cors())
 
-app.use("/character", characterRouter)
+router.use("/character", characterRouter)
 
+app.use("/.netlify/functions/api", router)
+
+module.exports = app
 module.exports.handler = serverless(app)
